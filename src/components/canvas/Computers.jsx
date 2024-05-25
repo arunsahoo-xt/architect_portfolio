32fc849c7d2e28b18e0fa779b0e68cbed4b0a3e8
arunsahoo-xt <armodilo60@gmail.com>
2024-05-25 21:44:16 +0530

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,74 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import * as THREE from 'three';
+
+import CanvasLoader from "../Loader";
+
+const HouseModel = ({ isMobile }) => {
+  const { scene } = useGLTF("./modern_house/scene.gltf", true);
+  const houseModel = scene.clone(); // Clone the scene to avoid modifying the original
+
+  return (
+    <group>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={houseModel}
+        scale={isMobile ? 0.7 : 0.25}
+        position={isMobile ? [0, -3, -2.2] : [0, -4.75, 2.5]}
+        rotation={[0, 0, 0]}
+      />
+    </group>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [-20, 0, 10], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 3}
+        />
+        <HouseModel isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
